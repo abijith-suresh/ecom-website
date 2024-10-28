14b81f68fe82c8782dee316c631940e815a42752
Abijith S <abijithsuresh47@gmail.com>
2024-10-28 11:09:35 +0530

    Added alert box for adding items to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
+import CartAlert from "./components/CartAlert";
 import products from "./data/products";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
-  const [productList, setProductList] = useState(products); // Initialize with existing products
+  const [productList, setProductList] = useState(products);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   const addToCart = (product) => {
     setCartItems((prevItems) => [...prevItems, product]);
+    setAlertMessage(`${product.name} has been added to your cart!`);
+    setIsAlertOpen(true);
+
+    setTimeout(() => {
+      setIsAlertOpen(false);
+    }, 1500);
   };
 
   const handleDelete = (index) => {
@@ -30,6 +39,11 @@ function App() {
         onAddProduct={handleAddProduct}
       />
       <HomePage products={productList} addToCart={addToCart} />
+      <CartAlert
+        message={alertMessage}
+        isOpen={isAlertOpen}
+        onClose={() => setIsAlertOpen(false)}
+      />
     </>
   );
 }
